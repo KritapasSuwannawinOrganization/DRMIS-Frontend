c02541fbb8b0ea8ec7cda7e03532b587dfb11d03
KritapasSuwannawin <kritapas.ms@gmail.com>
2023-05-09 07:44:20 +0700

    implement activity modal

--- src/pages/activity/Activity.js
+++ src/pages/activity/Activity.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import ScrollContainer from 'react-indiana-drag-scroll';
 
@@ -9,14 +10,40 @@ import drmisActivity from '../../icons/drmis-activity.svg';
 function Activity() {
   const activityArr = useSelector((store) => store.resource.activityArr);
 
+  const [modalImageURL, setModalImageURL] = useState();
+
+  function imageClickHandler() {
+    setModalImageURL(this);
+  }
+
+  function closeClickHandler() {
+    setModalImageURL();
+  }
+
   return (
     <div className="activity">
+      {modalImageURL && (
+        <div className="activity__modal-container">
+          <div className="modal">
+            <img src={modalImageURL} alt=""></img>
+            <div className="close-btn" onClick={closeClickHandler}>
+              <div className="line slash"></div>
+              <div className="line back-slash"></div>
+            </div>
+          </div>
+        </div>
+      )}
       <img className="activity__title" src={drmisActivity} alt=""></img>
       <div className="activity__container">
         <ScrollContainer className="content">
           {activityArr.map((activity) => (
             <div key={activity.id} className="activity-card">
-              <img src={pathToUrl(activity.poster_file_path)} alt="" className={activity.title ? 'short' : ''}></img>
+              <img
+                src={pathToUrl(activity.poster_file_path)}
+                alt=""
+                className={activity.title ? 'short' : ''}
+                onClick={imageClickHandler.bind(pathToUrl(activity.poster_file_path))}
+              ></img>
               {activity.title &&
                 (activity.link ? (
                   <a href={activity.link} target="_blank" rel="noreferrer">
